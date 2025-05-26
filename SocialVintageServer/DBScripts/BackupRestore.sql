fb955c743071139d6b20223cd99916bc2330df4c
amitb <amitb@DUDUB-X1-GEN5>
2025-05-26 19:43:00 +0300

    כחח

---
+++ SocialVintageServer/DBScripts/BackupRestore.sql
@@ -0,0 +1,43 @@
+﻿-- REPLACE YOUR DATABASE NAME, LOGIN AND PASSWORD IN THE SCRIPT BELOW
+
+USE master;
+GO
+
+-- Declare the database name
+DECLARE @DatabaseName NVARCHAR(255) = 'SocialVintageDB';
+
+-- Generate and execute the kill commands for all active connections
+DECLARE @KillCommand NVARCHAR(MAX);
+
+SET @KillCommand = (
+    SELECT STRING_AGG('KILL ' + CAST(session_id AS NVARCHAR), '; ')
+    FROM sys.dm_exec_sessions
+    WHERE database_id = DB_ID(@DatabaseName)
+);
+
+IF @KillCommand IS NOT NULL
+BEGIN
+    EXEC sp_executesql @KillCommand;
+    PRINT 'All connections to the database have been terminated.';
+END
+ELSE
+BEGIN
+    PRINT 'No active connections to the database.';
+END
+Go
+
+IF EXISTS (SELECT * FROM sys.databases WHERE name = N'SocialVintageDB')
+BEGIN
+    DROP DATABASE SocialVintageDB;
+END
+Go
+-- Create a login for the admin user
+CREATE LOGIN [AdminLogin] WITH PASSWORD = 'amitbe1011!';
+Go
+
+--so user can restore the DB!
+ALTER SERVER ROLE sysadmin ADD MEMBER [AdminLogin];
+Go
+
+CREATE Database SocialVintageDB;
+Go
